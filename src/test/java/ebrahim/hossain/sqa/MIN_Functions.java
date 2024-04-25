3d662ce5fdfb1b2c7a0cfc93c1ac48bc676b3d18
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-04-25 07:53:55 -0400

    add min function example

--- src/test/java/ebrahim/hossain/sqa/MIN_Functions.java
+++ src/test/java/ebrahim/hossain/sqa/MIN_Functions.java
@@ -1,5 +1,38 @@
 package ebrahim.hossain.sqa;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.testng.annotations.Test;
+
 public class MIN_Functions {
+
+	@Test
+	public void min() {
+		String url = "jdbc:mysql://localhost:3306/test";
+		String userName = "root";
+		String password = "root";
+
+		// SQL query with MIN() function
+		String selectSql = "SELECT MIN(age) AS min_age FROM student";
+
+		try (Connection conn = DriverManager.getConnection(url, userName, password);
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(selectSql)) {
+
+			// Print the minimum age of students
+			System.out.println("Minimum Age of Students:");
+			System.out.println("-------------------------");
+			if (rs.next()) {
+				int minAge = rs.getInt("min_age");
+				System.out.println("Minimum Age: " + minAge);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
