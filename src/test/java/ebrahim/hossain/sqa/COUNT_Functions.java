8e1016d07d62e8cc9f6b356aaa8df6e9a7239da4
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-04-25 08:43:40 -0400

    add count function example

---
+++ src/test/java/ebrahim/hossain/sqa/COUNT_Functions.java
@@ -0,0 +1,38 @@
+package ebrahim.hossain.sqa;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.testng.annotations.Test;
+
+public class COUNT_Functions {
+
+	@Test
+	public void count() {
+		String url = "jdbc:mysql://localhost:3306/test";
+		String userName = "root";
+		String password = "root";
+
+		// SQL query with COUNT() function
+		String selectSql = "SELECT COUNT(*) AS total_students FROM student";
+
+		try (Connection conn = DriverManager.getConnection(url, userName, password);
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(selectSql)) {
+
+			// Print the total number of students
+			System.out.println("Total Number of Students:");
+			System.out.println("-------------------------");
+			if (rs.next()) {
+				int totalStudents = rs.getInt("total_students");
+				System.out.println("Total Students: " + totalStudents);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
