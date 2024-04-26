f6c69aefa5f25a47df603b87dccbe8c0f9a7b54c
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-04-25 22:56:21 -0400

    add like operator example

--- src/test/java/ebrahim/hossain/sqa/LIKE_Operator.java
+++ src/test/java/ebrahim/hossain/sqa/LIKE_Operator.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 
 public class LIKE_Operator {
 
-	@Test (priority = 0)
+	@Test(priority = 0)
 	public void likeExample1() {
 		String url = "jdbc:mysql://localhost:3306/test";
 		String userName = "root";
@@ -26,6 +26,33 @@ public class LIKE_Operator {
 			// Print the students whose first name starts with 'J'
 			System.out.println("Students whose first name starts with 'J':");
 			System.out.println("------------------------------------------");
+			while (rs.next()) {
+				String firstName = rs.getString("first_name");
+				String lastName = rs.getString("last_name");
+				System.out.println(firstName + " " + lastName);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test(priority = 1)
+	public void likeExample2() {
+		String url = "jdbc:mysql://localhost:3306/test";
+		String userName = "root";
+		String password = "root";
+
+		// Search for students whose last name contains 'Brown'
+		String selectSql = "SELECT * FROM student WHERE last_name LIKE '%Brown%'";
+
+		try (Connection conn = DriverManager.getConnection(url, userName, password);
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(selectSql)) {
+
+			// Print the students whose last name contains 'Brown'
+			System.out.println("Students whose last name contains 'Brown':");
+			System.out.println("--------------------------------------------");
 			while (rs.next()) {
 				String firstName = rs.getString("first_name");
 				String lastName = rs.getString("last_name");
